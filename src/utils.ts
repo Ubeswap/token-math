fb53278582315dd4bf826070b7b84de0bfc470c7
Ian Macalinao <ian@ubeswap.org>
2022-07-13 14:02:51 -0500

    remove BN dependency

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,35 @@
-import BN from "bn.js";
 import { default as JSBI } from "jsbi";
 
 import { TEN } from "./constants.js";
 
 /**
+ * A type that is structurally compatible with BN.
+ */
+export type BNLike = {
+  toString(base?: number | "hex", length?: number): string;
+  gcd(b: BNLike): BNLike;
+  egcd(b: BNLike): { a: BNLike; b: BNLike; gcd: BNLike };
+  invm(b: BNLike): BNLike;
+};
+
+const BN_WORD_SIZE = 26;
+
+/**
+ * Checks if an object is a BN.
+ */
+export const isBN = (num: BNLike) => {
+  return (
+    num !== null &&
+    typeof num === "object" &&
+    (num.constructor as { wordSize?: number }).wordSize === BN_WORD_SIZE &&
+    Array.isArray((num as { words?: unknown }).words)
+  );
+};
+
+/**
  * Bigint-like number.
  */
-export type BigintIsh = JSBI | string | number | bigint | BN;
+export type BigintIsh = JSBI | string | number | bigint | BNLike;
 
 /**
  * Parses a {@link BigintIsh} into a {@link JSBI}.
@@ -16,7 +39,9 @@ export type BigintIsh = JSBI | string | number | bigint | BN;
 export function parseBigintIsh(bigintIsh: BigintIsh): JSBI {
   return bigintIsh instanceof JSBI
     ? bigintIsh
-    : typeof bigintIsh === "bigint" || BN.isBN(bigintIsh)
+    : typeof bigintIsh === "string" || typeof bigintIsh === "number"
+    ? JSBI.BigInt(bigintIsh)
+    : typeof bigintIsh === "bigint" || isBN(bigintIsh)
     ? JSBI.BigInt(bigintIsh.toString())
     : JSBI.BigInt(bigintIsh);
 }
